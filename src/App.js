cc7eccc969be9af7d4e7f1aa607c9a8e96c4465e
himalay123456 <himalay@solulab.co>
2024-03-01 13:56:25 +0530

    Apollo sandbox setup done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { ApolloSandbox } from '@apollo/sandbox/react';
+import "./index.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='apollo-container' style={{ height: "100vh"}}>
+      <ApolloSandbox
+        initialEndpoint="http://54.167.207.218:4003/graphql"
+        // includeCookies= false
+      />
     </div>
   );
 }
